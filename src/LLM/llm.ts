4696932b014f712de3d8514e059832575343b3e2
Ishan <ishan.sandhu3121@gmail.com>
2025-11-25 23:49:31 -0800

    working on file search for explain command

--- src/LLM/llm.ts
+++ src/LLM/llm.ts
@@ -1,10 +1,17 @@
 import { OpenRouter } from '@openrouter/sdk';
 import { readConfigFile } from '../config/configMain.js';
 import {Messages} from './MessageInterface.js';
-
+import fs from 'node:fs';
 
 export async function chat(filePath: string): Promise<string>{
   console.log(`File path: ${filePath}`);
+  // let systemPrompt: string = '';
+  // try{
+  //   systemPrompt = fs.readFileSync('src/commands/explain/sysPrompt.md', 'utf8');
+  // }catch(error){
+  //   console.error(`Error reading system prompt: ${error}`);
+  // }
+  // console.log(systemPrompt);
   const config = await readConfigFile();
   console.log(config);
   const ApiKey: string = config.apiKey;
--- src/commands/explain/explainCommand.ts
+++ src/commands/explain/explainCommand.ts
@@ -1,6 +1,8 @@
 import { chat } from "../../LLM/llm.js";
-// import { Messages } from "../../LLM/MessageInterface.js";
+import { Messages } from "../../LLM/MessageInterface.js";
 import { Command } from "commander";
+import fs from 'node:fs';
+import path from 'node:path';
 
 export default function explainCommand(program: Command){
 
@@ -8,10 +10,30 @@ export default function explainCommand(program: Command){
     .description('Explain full code file or specific code block (line numbers)')
     .option('-f, --file <filePath>', 'Path to the code file to explain')
     .action(async (options)=>{
+
+        let systemPrompt: string = '';
+        let fileContent: string = '';
+        let filePath: string = '';
+        try{
+            systemPrompt = fs.readFileSync('src/commands/explain/sysPrompt.md', 'utf8');
+            // fileContent = fs.readFileSync(path.resolve(options.file));
+            filePath = path.resolve(options.file);
+            console.log(`File path: ${filePath}`);
+            console.log(`parent path: ${path.dirname(options.file)}`);
+            fileContent = fs.readFileSync(path.resolve(options.file), 'utf8');
+            console.log(`File content: ${fileContent}`);
+        }catch(error){
+            console.error(`Error reading system prompt: ${error}`);
+        }
+        // console.log(`System prompt from explain command: ${systemPrompt}`);
+        const messages: Messages[] =[
+            {'role': 'system', 'content': systemPrompt},
+            {'role':'user', 'content': `Explain the code: ${fileContent}`}
+        ]
         console.log(options);
         if('file' in options){
-            const response = await chat(options.file);
-            console.log(response);
+            // const response = await chat(options.file);
+            // console.log(response);
         }else{
             console.error('No file path provided');
         }
